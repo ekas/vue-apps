9cd1f477b2c49c1d66cd43982b1515797e0cc952
ekas <ekaspreet93.singh@gmail.com>
2018-01-20 23:35:44 +0530

    vue games arena app

---
+++ vue-games-arena/src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Friends from '@/components/Friends'
+import Contact from '@/components/Contact'
+import Account from '@/components/Account'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/friends',
+      name: 'Friends',
+      component: Friends
+    },
+    {
+      path: '/contact',
+      name: 'Contact',
+      component: Contact
+    },
+    {
+      path: '/account',
+      name: 'Account',
+      component: Account
+    }
+  ]
+})
